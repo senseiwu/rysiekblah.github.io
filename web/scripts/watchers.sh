622e92ee860f432d2b9fd4d1e1913096048ffa4a
Tomek Kozlowski <tkozlowski007@gmail.com>
2016-10-15 11:49:56 +0800

    landing page

--- web/scripts/watchers.sh
+++ web/scripts/watchers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 exec `coffee --watch --compile --output gen/javascripts/ assets/javascripts/` &
 exec `sass --watch assets/stylesheets/:gen/stylesheets/ assets/stylesheets/lib/:gen/stylesheets/lib` &
+
+# run http server: https://www.npmjs.com/package/http-server
+`http-server .`
